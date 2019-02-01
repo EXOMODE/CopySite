bcdb6e466fc0af5215a3445f3aeb92a697931005
EXOMODE <exomode@outlook.com>
2019-02-01 13:40:18 +0500

    Добавьте файлы проекта.

---
+++ CopySite/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace CopySite
+{
+    internal static class Program
+    {
+        private static string outputPath;
+        private static string url;
+
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "-output":
+                    case "-o":
+                        outputPath = args[i + 1].Trim('"', '\'');
+
+                        if (!Directory.Exists(outputPath)) Directory.CreateDirectory(outputPath);
+
+                        i++;
+                        break;
+
+                    case "-url":
+                    case "-u":
+                        url = args[i + 1].Trim('"', '\'');
+                        i++;
+                        break;
+                }
+            }
+        }
+    }
+}
